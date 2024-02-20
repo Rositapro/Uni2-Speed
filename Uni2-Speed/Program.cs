fff0ad145a6cc65a83cf2866d73612a1ba55eb41
abrah <abrah@HPDEABRAHAM>
2024-02-19 23:43:55 -0600

    Agregar archivos de proyecto.

---
+++ Uni2-Speed/Program.cs
@@ -0,0 +1,9 @@
+﻿
+using Uni2_Speed;
+
+Console.WriteLine("enter the distance:");
+double distance = double.Parse(Console.ReadLine());
+Console.WriteLine("enter the time :");
+double time = double.Parse(Console.ReadLine());
+Speed speed = new Speed(distance, time);
+Console.WriteLine(speed.ToString());
---
+++ Uni2-Speed/Speed.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uni2_Speed
+{
+    public class Speed
+    {
+        double distance;
+        double time;
+        double speed;
+
+        //constructor
+        public Speed(double distance, double time)
+        {
+            this.distance = distance;
+            this.time = time;
+            speed= 0;
+            operacion();
+        }
+        public override string ToString()
+        {
+            return "the speed is: " + speed.ToString();
+        }
+
+        public void operacion ()
+        {
+          speed =  (distance / time);
+        }
+
+    }
+}
